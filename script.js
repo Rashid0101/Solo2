9cd6043243b0de52856cbeac17dc779c6e245e26
Rashid <127450565+Rashid0101@users.noreply.github.com>
2024-08-20 14:11:39 +0300

    finshed Javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let convertBtn = document.getElementById("convert-btn");
+let inputEl = document.getElementById("input-number");
+let lengthEl = document.getElementById("length-el-meters");
+let lengthElLiters = document.getElementById("length-el-liters");
+let lengthElMass = document.getElementById("length-el-mass");
+
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kiloToPound = 2.204;
+
+convertBtn.addEventListener("click", function () {
+  let baseValue = inputEl.value;
+  lengthEl.textContent = `${baseValue} meters = ${
+    baseValue * meterToFeet
+  } feet | ${baseValue} feet = ${baseValue * meterToFeet} meters`;
+});
+
+convertBtn.addEventListener("click", function () {
+  let literValue = inputEl.value;
+  lengthElLiters.textContent = `${literValue} liters = ${
+    literValue * literToGallon
+  } gallons | ${literValue} gallons = ${literValue * literToGallon} liters`;
+});
+
+convertBtn.addEventListener("click", function () {
+  let massValue = inputEl.value;
+  lengthElMass.textContent = `${massValue} kilos = ${
+    massValue * kiloToPound.toFixed(2)
+  } pounds | ${massValue} pounds = ${massValue * kiloToPound.toFixed(2)} kilos`;
+});
